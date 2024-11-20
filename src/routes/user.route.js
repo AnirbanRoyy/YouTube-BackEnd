f3ccb2598823d485191062752bae2789b324712d
AnirbanRoyy <anirban15987@gmail.com>
2024-11-20 19:06:04 +0530

    since no data is coming from user, we can use get

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -36,7 +36,7 @@ router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/get-user").post(verifyJWT, getCurrentUser);
+router.route("/get-user").get(verifyJWT, getCurrentUser);
 router.route("/update-user-details").patch(verifyJWT, updateUserDetails);
 router.route("/get-user-channel-profile").post(verifyJWT, getUserChannelProfile)
 
