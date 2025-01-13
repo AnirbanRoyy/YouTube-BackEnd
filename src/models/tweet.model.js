23876a2996a407d80f4b917b550d32f9455bca10
AnirbanRoyy <anirban15987@gmail.com>
2025-01-13 12:26:06 +0530

    add mongoose-aggregate-paginate-v2 as a plugin to tweets model

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const tweetSchema = new mongoose.Schema({
     owner: {
@@ -11,5 +12,7 @@ const tweetSchema = new mongoose.Schema({
         type: String,
     }
 }, { timestamps: true });
+
+tweetSchema.plugin(mongooseAggregatePaginate)
 
 export const Tweet = mongoose.model("Tweet", tweetSchema);
