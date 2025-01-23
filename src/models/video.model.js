433b205ba5b1b111ab19ed31dd25326e767fe850
AnirbanRoyy <anirban15987@gmail.com>
2025-01-23 17:41:23 +0530

    add thumbnail field

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -7,6 +7,10 @@ const videoSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        thumbnail: {
+            type: String,
+            required: true,
+        },
         title: {
             type: String,
             required: true,
