ae32584b13923b5a7007a127d51415c847749343
Anirban Roy <anirban.roy@bluevector.ai>
2025-07-12 19:55:49 +0530

    refactor: improve user-related routes and models

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -9,17 +9,22 @@ import {
     getCurrentUser,
     updateAvatar,
     updateCoverImage,
-    getUserChannelProfile,
+    getUserProfile,
     getWatchHistory,
     addToWatchHistory,
     getUserPlaylists,
+    getAllUsers,
+    getUser,
+    deleteUser,
+    deleteFromWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(
+// CRUD opeartions
+router.route("/").post(
     upload.fields([
         {
             name: "avatar",
@@ -32,29 +37,37 @@ router.route("/register").post(
     ]),
     registerUser
 );
+router.route("/").get(verifyJWT, getAllUsers);
+router.route("/:userId").get(verifyJWT, getUser);
+router.route("/:userId").patch(verifyJWT, updateUserDetails);
+router.route("/:userId").delete(verifyJWT, deleteUser);
 
+// Actions routes
 router.route("/login").post(loginUser);
 
 // secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
-router.route("/get-user").get(verifyJWT, getCurrentUser);
-router.route("/update-user-details").patch(verifyJWT, updateUserDetails);
-router
-    .route("/get-user-channel-profile/:username")
-    .post(verifyJWT, getUserChannelProfile);
+router.route("/get-current-user").post(verifyJWT, getCurrentUser);
+router.route("/get-user-profile/:username").post(verifyJWT, getUserProfile);
 
 router
     .route("/update-avatar")
     .patch(verifyJWT, upload.single("avatar"), updateAvatar);
 router
-    .route("/update-coverImage")
+    .route("/update-cover-image")
     .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
 
-router.route("/get-watch-history").get(verifyJWT, getWatchHistory);
+router.route("/get-watch-history").post(verifyJWT, getWatchHistory);
 
-router.route("/update-watch-history/:videoId").patch(verifyJWT, addToWatchHistory);
+router
+    .route("/add-to-watch-history/:videoId")
+    .post(verifyJWT, addToWatchHistory);
+
+router
+    .route("/remove-from-watch-history/:videoId")
+    .post(verifyJWT, deleteFromWatchHistory);
 
 router.route("/:userId/playlists").get(verifyJWT, getUserPlaylists);
 
